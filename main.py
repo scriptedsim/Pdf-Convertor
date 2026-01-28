c92c38694b6d5fcbb190f2a5329b2acc98783c7d
scriptedsim <simeenayman22@gmail.com>
2026-01-28 10:34:19 +0530

    Add lines

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font("Arial", size=16, style='B')
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=24, txt=row["Topic"], ln=1, align='L')
-    pdf.line(10, 28, 200, 28)
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align='L')
 
     #Set up footer
     pdf.ln(240)
     pdf.set_font("Arial", size=10, style='I')
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=24, txt=row["Topic"], ln=1, align='R')
+
+    for y in range(20, 298, 20):
+        pdf.line(10, y, 200, y)
+
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
@@ -29,6 +32,8 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=24, txt=row["Topic"], ln=1, align='R')
 
+        for y in range(20, 298, 20):
+            pdf.line(10, y, 200, y)
 
 
 pdf.output("output.pdf")
